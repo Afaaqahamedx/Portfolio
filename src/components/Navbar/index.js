3d1f214efdbe74f3ddc32e862ce10518775e1ada
Ahxmedxx <ajjam90321@gmail.com>
2023-12-21 16:06:19 +0530

    Changes in NavBar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,10 +12,23 @@ import {
   MobileMenu,
   MobileLink,
 } from "./NavbarStyledComponent";
+import styled from "styled-components";
 
 import { FaBars } from "react-icons/fa";
 import { Bio } from "../../data/constants";
 import { useTheme } from "styled-components";
+
+const MobileMenuWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  ${"" /* background: rgba(0, 0, 0, 0.5); */}
+  backdrop-filter: blur(5px); /* Adjust the blur amount as needed */
+  z-index: 999; /* Ensure the backdrop is above other elements */
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+`;
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -34,7 +47,6 @@ const Navbar = () => {
               cursor: "pointer",
             }}
           >
-          
             <Span>Afaaqahamed Jamadar</Span>
           </a>
         </NavLogo>
@@ -60,70 +72,72 @@ const Navbar = () => {
           </GitHubButton>
         </ButtonContainer>
 
-
         {isOpen && (
-          <MobileMenu isOpen={isOpen}>
-            <MobileLink
-              href="#about"
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            >
-              About
-            </MobileLink>
+          <>
+            <MobileMenuWrapper
+              isOpen={isOpen}
+              onClick={() => setIsOpen(!isOpen)}
+            />
+            <MobileMenu isOpen={isOpen}>
+              <MobileLink
+                href="#about"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                About
+              </MobileLink>
 
+              <MobileLink
+                href="#skills"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Skills
+              </MobileLink>
 
-            <MobileLink
-              href="#skills"
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            >
-              Skills
-            </MobileLink>
+              <MobileLink
+                href="#projects"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Projects
+              </MobileLink>
 
+              <MobileLink
+                href="#education"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Education
+              </MobileLink>
 
-            <MobileLink
-              href="#projects"
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            >
-              Projects
-            </MobileLink>
+              <MobileLink
+                href="#contact"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Contact
+              </MobileLink>
 
-            
-            <MobileLink
-              href="#education"
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            >
-              Education
-            </MobileLink>
-
-            <MobileLink
-              href="#contact"
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            >
-              Contact
-            </MobileLink>
-
-            <GitHubButton
-              style={{
-                padding: "10px 16px",
-                background: `${theme.primary}`,
-                color: "white",
-                width: "max-content",
-              }}
-              href={Bio.github}
-              target="_blank"
-            >
-              Github Profile
-            </GitHubButton>
-          </MobileMenu>
+              <GitHubButton
+                style={{
+                  padding: "10px 16px",
+                  background: `${theme.primary}`,
+                  color: "white",
+                  width: "max-content",
+                }}
+                href={Bio.github}
+                target="_blank"
+              >
+                Github Profile
+              </GitHubButton>
+            </MobileMenu>
+          </>
         )}
       </NavbarContainer>
     </Nav>
